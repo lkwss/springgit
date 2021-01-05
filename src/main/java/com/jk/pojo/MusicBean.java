09102fd091068d5d191abfc153380a3a86151ba3
lkw <1458462736@qq.com>
2021-01-05 20:04:42 +0800

    实体类

--- src/main/java/com/jk/pojo/MusicBean.java
+++ src/main/java/com/jk/pojo/MusicBean.java
@@ -1,4 +1,25 @@
 package com.jk.pojo;
 
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Transient;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
+@Data
+@Document(indexName = "t_music",type="music")
 public class MusicBean {
+    @Id
+    private Integer id;
+    @Field(type = FieldType.Text,analyzer = "ik_smart",copyTo = "detail")
+    private String musicname;
+    private String author;
+    private String launchtime;
+    private Double price;
+    private String info;
+    @Transient
+    private String mindate;
+    @Transient
+    private String maxdate;
 }
